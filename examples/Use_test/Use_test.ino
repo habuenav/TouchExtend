4b07690529fbadece34997ae39b5eb032004f778
Holman Buenaventura <habuenav@hotmail.com>
2025-05-16 14:32:04 -0500

    Update Use_test.ino

--- examples/Use_test/Use_test.ino
+++ examples/Use_test/Use_test.ino
@@ -1,30 +1,21 @@
-#include <tb.h>
+#include <touchExt.h>
+// Please don't use delay() function in your code 
+#define TOUCH_UMBRAL 60
+//#define touchExt tb
 
-#define pin1 4 
-#define pin2 2 
-tb button1(pin1);
-tb button2(pin2, 60); //second value is threshold
+touchExt button1(0, TOUCH_UMBRAL);
+touchExt button2(39,400,ADC_PULLDOWN);
+touchExt button3(1,4000,ADC_PULLUP);
 
-void setup() {
+
+void setup() 
+{
    Serial.begin(115200);
 }
 
-void loop() {
-
-  //Single tap
-  if(button1.isClick()) Serial.println("Single tap");
-  //Double tap
-  if(button1.isClicks(2)) Serial.println("Double tap");
-  //Triple tap
-  if(button1.isClicks(3)) Serial.println("Triple tap");
-
-  // Do anything for long time when button is hold
-  if(button1.isHold()) Serial.println("Hold");
-
-  // Do anything for long time when button2 is hold
-  if(button2.isHolded()) Serial.println("Holded");
-  
-  // Do anything one time when button was release
-    if(button2.isRelease()){ Serial.println("Release");}
-
+void loop() 
+{
+   if(button1.isTouched()  ){  Serial.println("Touch button 1");    }
+   if(button2.isClick()){   Serial.println("click button 2");    }
+   if(button3.isReleased()){   Serial.println("relase button3");    }
 }
